96cfa4993064b329c4d71b96c546e76e6f476f58
Babet <134620140+Bete123@users.noreply.github.com>
2024-07-10 14:30:01 +0700

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,28 @@
+document.querySelectorAll('.btnDetail').forEach(item => {
+    item.addEventListener('click', (e) => {
+        let parent = e.target.parentNode.parentNode;
+
+        let gambar = parent.querySelector('.card-img-top').src;
+        let harga = parent.querySelector('.harga').innerHTML;
+        let judul = parent.querySelector('.card-text').innerHTML;
+        let deskripsi = parent.querySelector('.deskripsi') ? parent.querySelector('.deskripsi').innerHTML : '<i>tidak ada informasi yang tersedia</i>';
+
+
+        let tombolModal = document.querySelector('.btnModal');
+        tombolModal.click();
+
+        document.querySelector('.modalTitle').innerHTML = judul;
+        let image = document.createElement('img');
+        image.src = gambar;
+        image.classList.add('w-100');
+        document.querySelector('.modalImage').innerHTML = '';
+        document.querySelector('.modalImage').appendChild(image);
+        document.querySelector('.modalDeskripsi').innerHTML = deskripsi;
+        document.querySelector('.modalHarga').innerHTML = harga;
+
+        const nohp = '6285757476743';
+        let pesan = `https://api.whatsapp.com/send?phone=${nohp}&text=Halo, saya mau pesan produk ini ${gambar}`;
+
+        document.querySelector('.btnBeli').href = pesan;
+    });
+});
